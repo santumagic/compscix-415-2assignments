e45a8d95a95b1643f9295c18fd7b8cfcaad2c75d
santoshk <santumagic@gmail.com>
2018-07-03 09:22:15 -0700

    draft

---
+++ Edx.R
@@ -0,0 +1,85 @@
+# Variables
+3+4
+"Hi There, Console"
+2
+height <- 2
+height
+width <- 4
+width
+ls()
+area <- height*width
+area
+ls()
+rm(area)
+ls()
+ls()
+area
+rm()
+rm(height)
+
+# Basic Data Types
+
+class()
+TRUE
+class(TRUE)
+FALSE
+class(FALSE)
+T
+F
+2
+2.5
+2L
+class(2L)
+class(2)
+is.numeric(2)
+is.numeric(2L)
+k<-"I Love"
+class(k)
+is.numeric(k)
+is.character(k)
+as.numeric(4)
+as.character(4.5)
+as.numeric(4.5)
+
+# Create and Name vectors
+
+c()
+names()
+str()
+
+
+# Vector Arithmetic
+# Subsetting Vectors
+# Create and Name Metrices
+
+stamatrix()
+
+(matrix_ex <- matrix(1:6, nrow = 2))
+(matrix_ex1 <- matrix(1:6, ncol = 3))
+(matrix_ex2 <- matrix(1:6, nrow = 2, byrow = TRUE))
+
+cbind(1:3, 1:6)
+rbind(1:3, 1:3)
+
+rownames()
+colnames()
+
+
+
+
+## Matrix Subsetting
+
+(m <- matrix(sample(1:15, 12), nrow = 3))
+
+
+
+
+
+
+
+
+
+
+
+
+  
